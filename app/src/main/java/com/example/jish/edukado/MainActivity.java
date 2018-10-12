05b9ab6a10699bf833b2c223ba00e1a1e4fc9a81
jish98 <ajishinnovations@gmail.com>
2018-10-12 20:20:05 +0530

    Initial setup

--- app/src/main/java/com/example/jish/edukado/MainActivity.java
+++ app/src/main/java/com/example/jish/edukado/MainActivity.java
@@ -1,101 +1,43 @@
-package com.example.jish.textdetector;
+package com.example.jish.edukado;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.os.Bundle;
-import android.provider.MediaStore;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.ml.vision.FirebaseVision;
-import com.google.firebase.ml.vision.common.FirebaseVisionImage;
-import com.google.firebase.ml.vision.text.FirebaseVisionText;
-import com.google.firebase.ml.vision.text.FirebaseVisionTextDetector;
+ import android.os.Bundle;
+ import android.app.Activity;
+ import android.content.Intent;
+ import android.view.Menu;
+ import android.view.View;
+ import android.view.View.OnClickListener;
+ import android.widget.Button;
 
-import java.util.List;
+    public class MainActivity extends Activity implements OnClickListener {
 
-public class MainActivity extends AppCompatActivity {
+        Button b1;
 
-    private Button snapBtn;
-    private Button detectBtn;
-    private ImageView imageView;
-    private TextView txtView;
-    private Bitmap imageBitmap;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            b1 = (Button)findViewById(R.id.button1);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        snapBtn = findViewById(R.id.snapBtn);
-        detectBtn = findViewById(R.id.detectBtn);
-        imageView = findViewById(R.id.imageView);
-        txtView = findViewById(R.id.txtView);
-        snapBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dispatchTakePictureIntent();
-            }
-        });
-        detectBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                detectTxt();
-            }
-        });
-    }
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
+            b1.setOnClickListener(this);
 
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
-    }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            imageBitmap = (Bitmap) extras.get("data");
-            imageView.setImageBitmap(imageBitmap);
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+
+            getMenuInflater().inflate(R.menu.main, menu);
+            return true;
         }
-    }
 
-    private void detectTxt() {
-        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(imageBitmap);
-        FirebaseVisionTextDetector detector = FirebaseVision.getInstance().getVisionTextDetector();
-        detector.detectInImage(image).addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
-            @Override
-            public void onSuccess(FirebaseVisionText firebaseVisionText) {
-                processTxt(firebaseVisionText);
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
+        @Override
+        public void onClick(View v) {
 
-            }
-        });
-    }
-
-    private void processTxt(FirebaseVisionText text) {
-        List<FirebaseVisionText.Block> blocks = text.getBlocks();
-        if (blocks.size() == 0) {
-            Toast.makeText(MainActivity.this, "No Text, Capture again :(", Toast.LENGTH_LONG).show();
-            return;
+            Intent i = new Intent(getApplicationContext(), MainActivity2.class);
+            startActivity(i);
+            setContentView(R.layout.avtivity_next);
         }
-        for (FirebaseVisionText.Block block : text.getBlocks()) {
-            String txt = block.getText();
-            txtView.setTextSize(24);
-            txtView.setText(txt);
-        }
-    }
 
-}
+
+    }
